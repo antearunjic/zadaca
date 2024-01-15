f8d230f2ca6b706d8fbe65f2d7dd1ba70a25e5c5
Antea Runjić <132346858+antearunjic@users.noreply.github.com>
2024-01-15 20:23:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import FunctionComponent from "./components/FunctionComponent";
 const App = () => {
   const ispis =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, expedita.";
+  const drugiIspis =
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi obcaecati incidunt magni quas debitis alias repellendus? Dolorem a dicta architecto.";
   return (
     <>
       <ClassComponent />
